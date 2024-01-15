cc099415f541241f836f4fc2af520adf48dda39d
dhianapereira <dhianaslv@outlook.com>
2024-01-15 18:11:10 -0300

    feat: Get products from API

--- shop/src/styles/pages/home.ts
+++ shop/src/styles/pages/home.ts
@@ -8,7 +8,7 @@ export const HomeContainer = styled('main', {
   minHeight: 656
 })
 
-export const Product = styled('a', {
+export const Product = styled('div', {
   background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
   borderRadius: 8,
   cursor: 'pointer',
@@ -43,7 +43,8 @@ export const Product = styled('a', {
     transition: 'all 0.2s ease-in-out',
 
     strong: {
-      fontSize: '$lg'
+      fontSize: '$lg',
+      color: '$gray100',
     },
 
     span: {
