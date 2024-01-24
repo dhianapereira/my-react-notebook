96968d6755e72d69e99710b331c3817d4c546a06
dhianapereira <dhianaslv@outlook.com>
2024-01-24 18:09:41 -0300

    feat: Add Next Auth

--- call/src/pages/register/connect-calendar/index.page.tsx
+++ call/src/pages/register/connect-calendar/index.page.tsx
@@ -1,12 +1,19 @@
 import { Button, Heading, MultiStep, Text } from "@ignite-ui/react";
 import { Container, Header } from "../styles";
-import { ArrowRight } from "phosphor-react";
-import { ConnectBox, ConnectItem } from "./styles";
+import { ArrowRight, Check } from "phosphor-react";
+import { AuthError, ConnectBox, ConnectItem } from "./styles";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 export default function Register() {
+    const session = useSession()
+    const router = useRouter()
 
-    async function handleRegister() {
+    let hasAuthError = !!router.query.error
+    const isSignedIn = session.status == 'authenticated'
 
+    async function handleConnectCalendar() {
+        await signIn('google')
     }
 
     return (
@@ -24,12 +31,36 @@ export default function Register() {
             <ConnectBox>
                 <ConnectItem>
                     <Text>Google Agenda</Text>
-                    <Button variant="secondary" size="sm">
-                        Conectar
-                        <ArrowRight />
-                    </Button>
+                    {
+                        isSignedIn ?
+                            (
+                                <Button size="sm" disabled>
+                                    Conectado
+                                    <Check />
+                                </Button>
+                            ) :
+                            (
+                                <Button
+                                    variant="secondary"
+                                    size="sm"
+                                    onClick={handleConnectCalendar}
+                                >
+                                    Conectar
+                                    <ArrowRight />
+                                </Button>
+                            )
+                    }
                 </ConnectItem>
-                <Button type="submit">
+                {
+                    hasAuthError &&
+                    (
+                        <AuthError size="sm">
+                            Falha ao se conectar ao Google, verifique se você habilitou as
+                            permissões de acesso ao Google Agenda.
+                        </AuthError>
+                    )
+                }
+                <Button type="submit" disabled={!isSignedIn}>
                     Próximo passo
                     <ArrowRight />
                 </Button>
